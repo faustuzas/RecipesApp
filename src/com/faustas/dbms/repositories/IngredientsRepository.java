5461ca2d4b795338d1c50bc5c051ca55feb56f1e
Faustas <faustas.butkus@gmail.com>
2019-05-10 15:13:14 +0300

    Method order changed

--- src/com/faustas/dbms/repositories/IngredientsRepository.java
+++ src/com/faustas/dbms/repositories/IngredientsRepository.java
@@ -9,17 +9,17 @@ import java.util.List;
 @Repository(Ingredient.class)
 public interface IngredientsRepository {
 
-    @Select("SELECT * FROM ingredients WHERE id = #id")
-    @Results({
-            @Result(column = "product_id", property = "product", exec = @Exec(aClass = ProductRepository.class))
-    })
-    Ingredient findById(@Param("id") Integer id);
-
     @Select("SELECT * FROM ingredients")
     @Results({
             @Result(column = "product_id", property = "product", exec = @Exec(aClass = ProductRepository.class))
     })
     List<Ingredient> findAll();
+
+    @Select("SELECT * FROM ingredients WHERE id = #id")
+    @Results({
+            @Result(column = "product_id", property = "product", exec = @Exec(aClass = ProductRepository.class))
+    })
+    Ingredient findById(@Param("id") Integer id);
 
     @Update("UPDATE ingredients " +
             "SET amount = #i.amount, product_id = #i.product.id " +
