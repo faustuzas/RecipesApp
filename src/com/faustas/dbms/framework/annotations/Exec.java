3395ab5dd88d59a12eb92384916ca662c182de57
Faustas <faustas.butkus@gmail.com>
2019-05-15 20:20:35 +0300

    Correct targets for annotations declared

--- src/com/faustas/dbms/framework/annotations/Exec.java
+++ src/com/faustas/dbms/framework/annotations/Exec.java
@@ -1,5 +1,9 @@
 package com.faustas.dbms.framework.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
 public @interface Exec {
     Class aClass() default void.class;
 
